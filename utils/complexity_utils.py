fix: metrics computation

utils/complexity_utils.py
@@ -296,8 +296,10 @@ def summary(input_size, model):
 
             params = 0
             if hasattr(module, 'weight'):
-                params += torch.prod(torch.LongTensor(list(module.weight.size())))
-                if module.weight.requires_grad:
+                if hasattr(module.weight, 'size'):
+                    params += torch.prod(torch.LongTensor(list(module.weight.size())))
+                
+                if hasattr(module.weight, 'requires_grad') and module.weight.requires_grad:
                     summary[m_key]['trainable'] = True
                 else:
                     summary[m_key]['trainable'] = False
@@ -329,24 +331,18 @@ def summary(input_size, model):
     for h in hooks:
         h.remove()
 
-    # print('----------------------------------------------------------------')
-    # line_new = '{:>20}  {:>25} {:>15}'.format('Layer (type)', 'Output Shape', 'Param #')
-    # print(line_new)
-    # print('================================================================')
     total_params = 0
     trainable_params = 0
     for layer in summary:
-        ## input_shape, output_shape, trainable, nb_params
-        line_new = '{:>20}  {:>25} {:>15}'.format(
-            layer, summary[layer]['output_shape'], summary[layer]['nb_params'])
         total_params += summary[layer]['nb_params']
         if 'trainable' in summary[layer]:
             if summary[layer]['trainable'] == True:
                 trainable_params += summary[layer]['nb_params']
-    #    print(line_new)
+    
     print('================================================================')
     print('Total params: ' + str(total_params))
     print('Trainable params: ' + str(trainable_params))
     print('Non-trainable params: ' + str(total_params - trainable_params))
     print('----------------------------------------------------------------')
+    
     return summary, total_params

utils/metrics.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import GPUtil
 from utils.complexity_utils import add_flops_counting_methods, summary
+import copy
 
 class AverageMeter:
     """
@@ -80,19 +81,20 @@ def accuracy(output, target, topk=(1,)):
     return res
 
 
-def compute_memory_usage(load_model_function, batch_sizes=[1, 2, 4, 8, 16, 32, 64]):
+def compute_memory_usage(model, input_size, batch_sizes=[1, 2, 4, 8, 16, 32, 64]):
     memory = []
     gpu = GPUtil.getGPUs()[0]
     empty_gpu = gpu.memoryUsed
 
-    model = load_model_function()
+    new_model = copy.deepcopy(model)
     for i, bs in enumerate(batch_sizes):
         with torch.no_grad():
-            _ = model(torch.randn(bs, *model.input_size).cuda(non_blocking=True))
+            _ = new_model(torch.randn(
+                bs, *input_size).cuda(non_blocking=True))
 
         gpu = GPUtil.getGPUs()[0]
         memory.append(gpu.memoryUsed)
-    del model
+    del new_model
     torch.cuda.empty_cache()
 
     return memory
@@ -153,5 +155,16 @@ def compute_complexity(model, input_size, batch_size=64):
         _ = model(torch.randn(batch_size, *
                         input_size).cuda(non_blocking=True))
 
-    summ, n_params = summary(input_size, model)
-    return model.compute_average_flops_cost() / 1e9 / 2, n_params.item()
+    return model.compute_average_flops_cost() / 1e9 / 2
+
+
+def compute_parameters(model):
+    total_params = 0
+    trainable_params = 0
+
+    for name, parameter in model.named_parameters():
+        param = parameter.numel()
+        total_params += param
+        if parameter.requires_grad:
+            trainable_params += param
+    return total_params, trainable_params