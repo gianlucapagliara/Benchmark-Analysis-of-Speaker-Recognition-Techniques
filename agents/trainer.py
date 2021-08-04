fix: metrics computation

agents/trainer.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import importlib
 import numpy as np
-import random
+import GPUtil
 
 import torch
 import torch.nn.functional as F
@@ -70,23 +70,24 @@ class Trainer(BaseAgent):
                 self.test_dataset,
                 batch_size=1,
                 num_workers=config.nDataLoaderThread,
-                pin_memory=True, # useful?
+                pin_memory=True,  # useful?
                 shuffle=False,
                 drop_last=False,
                 # sampler=self.test_sampler
             )
             self.test_normalize = False
 
-
+        self.required_memory = GPUtil.getGPUs()[0].memoryUsed
         # Model
         Model = importlib.import_module(
             'graphs.models.' + config.model).__getattribute__(config.model)
         self.__model__ = Model(**vars(config)).to(self.device)
+        self.required_memory = GPUtil.getGPUs()[0].memoryUsed - self.required_memory
 
         # Checkpoint Loading (if not found start from scratch)
         if(self.config.get('checkpoint_file', "") != ""):
             self.load_checkpoint(self.config.checkpoint_file)
-        
+
         # Model Loading (if not found start from scratch)
         if(self.config.get('initial_model', "") != ""):
             self.load_parameters(self.config.initial_model)
@@ -97,7 +98,6 @@ class Trainer(BaseAgent):
                 'graphs.losses.'+config.loss_function).__getattribute__('LossFunction')
             self.__loss__ = LossFunction(**vars(config)).to(self.device)
             self.test_normalize = self.__loss__.test_normalize
-
 
             # Optimizer
             Optimizer = importlib.import_module(
@@ -146,12 +146,12 @@ class Trainer(BaseAgent):
 
     def load_parameters(self, path):
         loaded_state = torch.load(path, map_location="cuda:%d" % self.gpu)
-        
+
         if loaded_state.get('state_dict', "") != "":
-            loaded_state=loaded_state['state_dict']
+            loaded_state = loaded_state['state_dict']
 
         self.__model__.load_state_dict(loaded_state)
-        
+
     def save_checkpoint(self, file_name="checkpoint.pth.tar", is_best=0):
         state = {
             'epoch': self.current_epoch,
@@ -192,21 +192,27 @@ class Trainer(BaseAgent):
         self.logger.info('Computing memory usage...')
         memory = compute_memory_usage(self.__model__, input_size)
 
-        self.logger.info('================================================================')
+        self.logger.info(
+            '================================================================')
         self.logger.info(
             str(self.__model__.__class__).split('.')[-1].split("'")[0])
-        self.logger.info('================================================================')
+        self.logger.info(
+            '================================================================')
         self.logger.info('Total params: ' + str(total_params))
         self.logger.info('Trainable params: ' + str(trainable_params))
-        self.logger.info('Non-trainable params: ' + str(total_params - trainable_params))
-        self.logger.info('----------------------------------------------------------------')
+        self.logger.info('Non-trainable params: ' +
+                         str(total_params - trainable_params))
+        self.logger.info(
+            '----------------------------------------------------------------')
         self.logger.info(f'Average flops cost: {avg_flops_cost}')
-        self.logger.info('----------------------------------------------------------------')
+        self.logger.info(
+            '----------------------------------------------------------------')
         self.logger.info('Average inference time for every batch size:')
         batch_sizes = [1, 2, 4, 8, 16, 32, 64]
         for b, m, s in zip(batch_sizes, mean_tfp, std_tfp):
             self.logger.info(f'{b}: {m:.2f} +/- {s:.2f}')
-        self.logger.info('----------------------------------------------------------------')
+        self.logger.info(
+            '----------------------------------------------------------------')
         self.logger.info('Memory usage for every batch size:')
         batch_sizes = [1, 2, 4, 8, 16, 32, 64]
         for b, m in zip(batch_sizes, memory):
@@ -355,7 +361,7 @@ class Trainer(BaseAgent):
                 mindcf, threshold = ComputeMinDcf(
                     fnrs, fprs, thresholds, p_target, c_miss, c_fa)
                 eer = result[1]
-            
+
             process_time = time.time()-start_time-loop_time
 
             # Logging
@@ -363,7 +369,7 @@ class Trainer(BaseAgent):
             perc_proc = process_time/total*100
 
             tqdm_test.set_description("Epoch-{} Validation | VEER {:2.4f}% MDC {:2.5f} | Efficiency {:2.2f}% "
-                                        .format(self.current_epoch+1, eer, mindcf, perc_proc))
+                                      .format(self.current_epoch+1, eer, mindcf, perc_proc))
 
             start_time = time.time()
 

datasets/AutoSpeechTestset.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from utils.transforms import Normalize, generate_test_sequence
 from utils.params import *
 
+
 def get_test_paths(pairs_path, db_dir):
     def convert_folder_name(path):
         basename = os.path.splitext(path)[0]
@@ -16,7 +17,8 @@ def get_test_paths(pairs_path, db_dir):
         p = os.path.join(speaker_dir, fname)
         return p
 
-    pairs = [line.strip().split() for line in open(pairs_path, 'r').readlines()]
+    pairs = [line.strip().split()
+             for line in open(pairs_path, 'r').readlines()]
     nrof_skipped_pairs = 0
     path_list = []
     issame_list = []
@@ -30,12 +32,13 @@ def get_test_paths(pairs_path, db_dir):
         path0 = db_dir.joinpath(convert_folder_name(pair[1]))
         path1 = db_dir.joinpath(convert_folder_name(pair[2]))
 
-        if os.path.exists(path0) and os.path.exists(path1):    # Only add the pair if both paths exist
-            path_list.append((path0,path1,issame))
+        # Only add the pair if both paths exist
+        if os.path.exists(path0) and os.path.exists(path1):
+            path_list.append((path0, path1, issame))
             issame_list.append(issame)
         else:
             nrof_skipped_pairs += 1
-    if nrof_skipped_pairs>0:
+    if nrof_skipped_pairs > 0:
         print('Skipped %d image pairs' % nrof_skipped_pairs)
 
     return path_list
@@ -58,6 +61,12 @@ class AutoSpeechTestset(Dataset):
     def load_feature(self, feature_path):
         feature = np.load(feature_path)
         test_sequence = generate_test_sequence(feature, self.partial_n_frames)
+
+        test_sequence = test_sequence[0, :, :]
+
+        if self.transform is not None:
+            test_sequence = self.transform(test_sequence)
+
         return test_sequence
 
     def __getitem__(self, index):
@@ -66,9 +75,6 @@ class AutoSpeechTestset(Dataset):
         feature1 = self.load_feature(path_1)
         feature2 = self.load_feature(path_2)
 
-        if self.transform is not None:
-            feature1 = self.transform(feature1)
-            feature2 = self.transform(feature2)
         return feature1, feature2, issame
 
     def __len__(self):

graphs/models/SincNet.py
@@ -66,12 +66,20 @@ class SincNet(BaseModel):
         if state_dict.get('DNN2_model_par', '') != '':
             self.DNN2_net.load_state_dict(state_dict['DNN2_model_par'])
 
-    def preforward(self, x):
-        x = x.squeeze(0)
-        return x
+    def scoring(self, ref, com, normalize=False):
+        # Feature extraction
+        ref_feat = self.get_dvect(ref).to(self.device)
+        com_feat = self.get_dvect(com).to(self.device)
 
-    def forward(self, x):
-        signal = self.preforward(x)
+        # Distance
+        score = F.pairwise_distance(ref_feat, com_feat)
+        score = score.detach().cpu().numpy()
+        score = -1 * np.mean(score)
+
+        return score
+
+    def get_dvect(self, signal):
+        signal = signal.squeeze(0)
 
         # split signals into chunks
         beg_samp = 0
@@ -98,7 +106,7 @@ class SincNet(BaseModel):
             if count_fr == self.batch_size:
                 inp = Variable(sig_arr)
                 out = self.DNN1_net(self.CNN_net(inp))
-                dvects[count_fr_tot-self.batch_size:count_fr_tot,:] = out
+                dvects[count_fr_tot-self.batch_size:count_fr_tot, :] = out
                 count_fr = 0
                 sig_arr = torch.zeros([self.batch_size, self.wlen]).float().to(
                     self.device).contiguous()
@@ -106,10 +114,25 @@ class SincNet(BaseModel):
         if count_fr > 0:
             inp = Variable(sig_arr[:count_fr])
             out = self.DNN1_net(self.CNN_net(inp))
-            dvects[count_fr_tot-count_fr-1:count_fr_tot,:] = out
+            dvects[count_fr_tot-count_fr-1:count_fr_tot, :] = out
 
         # averaging and normalizing all the d-vectors
-        d_vect_out = torch.mean(dvects/dvects.norm(p=2, dim=1).view(-1, 1), dim=0)
+        d_vect_out = torch.mean(
+            dvects/dvects.norm(p=2, dim=1).view(-1, 1), dim=0)
 
         d_vect_out = d_vect_out.unsqueeze(0)
         return d_vect_out
+
+    def preforward(self, x):
+        if x.shape == 1:
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, x):
+        x = self.preforward(x)
+        d_vects = []
+        for i in range(0, x.shape[0]):
+            signal = x[i, :]
+            d_vects.append(self.get_dvect(signal))
+        return d_vects
+

utils/metrics.py
@@ -81,20 +81,17 @@ def accuracy(output, target, topk=(1,)):
     return res
 
 
-def compute_memory_usage(model, input_size, batch_sizes=[1, 2, 4, 8, 16, 32, 64]):
+def compute_memory_usage(model, input_size, batch_sizes=[1, 2, 4, 8, 16, 32, 64], model_memory=0):
     memory = []
     gpu = GPUtil.getGPUs()[0]
     empty_gpu = gpu.memoryUsed
 
-    new_model = copy.deepcopy(model)
     for i, bs in enumerate(batch_sizes):
         with torch.no_grad():
-            _ = new_model(torch.randn(
+            _ = model(torch.randn(
                 bs, *input_size).cuda(non_blocking=True))
 
-        gpu = GPUtil.getGPUs()[0]
-        memory.append(gpu.memoryUsed)
-    del new_model
+        memory.append(model_memory + gpu.memoryUsed-empty_gpu)
     torch.cuda.empty_cache()
 
     return memory
@@ -152,8 +149,8 @@ def compute_complexity(model, input_size, batch_size=64):
     model.start_flops_count()
 
     with torch.no_grad():
-        _ = model(torch.randn(batch_size, *
-                        input_size).cuda(non_blocking=True))
+        x = torch.randn(batch_size, *input_size).cuda(non_blocking=True)
+        _ = model(x)
 
     return model.compute_average_flops_cost() / 1e9 / 2
 